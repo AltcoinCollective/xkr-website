b0838554848d826e3db213a78c39e92862c9b094
Lukas Nyberg <Lukas.nyberg@gmail.com>
2021-11-05 13:26:47 +0100

    fowrard

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,19 +1,32 @@
 import * as React from 'react'
-import { Container, Heading, Stack, Text } from '@chakra-ui/layout'
+import { Box, Container, Heading, Stack, Text } from '@chakra-ui/layout'
 import { Button } from '@chakra-ui/button'
 import { Img } from '@chakra-ui/react'
+import { background } from '@chakra-ui/styled-system';
 
 const Hero = () => {
     return (
-        <Stack padding="3" marginTop="5">
-                <Img src=""/>
-                <Heading align="center" fontSize="6xl">A Nordic cryptocurrency for the future</Heading>
-                <Text align="center" fontSize="md">On 2 April 2019, Kryptokrona was launched to secure and simplify our future economic system. Sending and receiving money should not be expensive or slow. We work with open source code that allows you to be involved and improve the money of the future.</Text>
-            <Stack padding="3" alignSelf="center" direction="row">
+    <Container className='rgb' maxW='7xl'>
+            <Img
+            className='rgb'
+            maxW='7xl'
+            src='https://kryptolink.se/static/xkrart-62cd1762a4c10098ca3b6b8dcca2c7f8.png'
+            position='relative'
+            objectFit='cover'
+            objectPosition='center center'
+            ></Img>
+        <Stack px="5" pt='10'>
+                <Heading align="Start" fontSize="6xl">A Nordic cryptocurrency for the future</Heading>
+                <Text align="start" fontSize="md">On 2 April 2019, Kryptokrona was launched to secure and simplify our future economic system. Sending and receiving money should not be expensive or slow. We work with open source code that allows you to be involved and improve the money of the future.</Text>
+            <Stack alignSelf="Start" direction="row">
                 <Button colorScheme="green" size="lg">Get Started</Button>
                 <Button colorScheme="green" variant="outline" size="lg">Download</Button>
             </Stack>
+        <Stack>
+            
         </Stack>
+        </Stack>
+    </Container>
     )
 }
 
